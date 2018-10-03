12cdb8e265b66625984678673f64cd73303d2b39
Michael Stewart <mstewart@mstewart-l.corp.instructure.com>
2018-10-02 19:40:33 -0600

    New files

---
+++ exersize.js
@@ -0,0 +1,6 @@
+var first = prompt("What is your first name?");
+var last = prompt("What is your last name");
+var age = prompt("What is your age?");
+
+alert("Hello " + first + " " + last + ". It's a beautiful day!");
+alert("I'm actually " + age + " too!");
